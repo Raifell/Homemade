951bcd75839b3a8d2bb15eca5b1a0c8a55d31545
Raifell <ArtSkvaer2@gmail.com>
2023-10-07 01:25:49 +0500

    add HW_07_10_2023

--- 10_2023/HW_07_10_2023/Task_4.js
+++ 10_2023/HW_07_10_2023/Task_4.js
@@ -5,12 +5,16 @@ alert('Задача 4 \nКонвертор валют. Пользователь 
 let conv = Number(prompt('Введите количество USD'));
 let cash = prompt('Введите валютный код для конвертации\nEUR, UAN, AZN\nРегистр не имеет значения');
 
-if(cash.toLowerCase()==='eur'){
-    alert(conv + ' USD = ' + (conv * 0.94794).toFixed(2) + ' EUR')
-} else if(cash.toLowerCase()==='uah'){
-    alert(conv + ' USD = ' + (conv * 36.63).toFixed(2) + ' UAH')
-} else if(cash.toLowerCase()==='azn'){
-    alert(conv + ' USD = ' + (conv * 1.7).toFixed(2) + ' AZN')
+if(conv!=null&&cash!=null){
+    if(cash.toLowerCase()==='eur'){
+        alert(conv + ' USD = ' + (conv * 0.94794).toFixed(2) + ' EUR')
+    } else if(cash.toLowerCase()==='uah'){
+        alert(conv + ' USD = ' + (conv * 36.63).toFixed(2) + ' UAH')
+    } else if(cash.toLowerCase()==='azn') {
+        alert(conv + ' USD = ' + (conv * 1.7).toFixed(2) + ' AZN')
+    } else {
+        alert('Неверные данные')
+    }
 } else {
     alert('Неверные данные')
 }
